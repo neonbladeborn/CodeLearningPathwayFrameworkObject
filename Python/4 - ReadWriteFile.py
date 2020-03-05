39051fb207d3fc309100d4644089dc52b1bc5c87
neonbladeborn <hussarwingsgaming@gmail.com>
2020-03-05 14:10:39 +1100

    added comments, updated sorting

--- Python/4 - ReadWriteFile.py
+++ Python/4 - ReadWriteFile.py
@@ -1,27 +1,21 @@
 unsortedList = open("4.1 - unsortedList", "r")
 arraylist = []
 
-for line in unsortedList:
-	arraylist.append(line)
+for line in unsortedList: #for all the lines in the list document
+	arraylist.append(line) #it adds that line to the end of the array
 
-sortedList = open("4.2 - sortedList", "w")
+sortedList = open("4.2 - sortedList", "w") 
 sortedList.seek(0) # goes to start of file
 sortedList.truncate() # deletes all data
 
+#loop to go through the array enough times to bubble sort
 for p in range(len(arraylist)):
-	for i in range(len(arraylist) - 1):
-		if (arraylist[i] > arraylist[i + 1]):
+	for i in range(len(arraylist) - 1): #not overshooting the array
+		if (int(arraylist[i]) > int(arraylist[i + 1])): #converting to int for sorting, remove int for alphabetical sorting 
+			#sorting 2 elements in array
 			temp = arraylist[i+1]
 			arraylist[i+1] = arraylist[i]
 			arraylist[i] = temp
 
-
-
-
-
 for x in arraylist:
-	sortedList.write(x)
-
-
-
-
+	sortedList.write(x) #writes all the elements into the array
